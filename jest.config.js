59aba8c3f0199e49baba8594500e9ed257adf1cb
Felipe Maia <felipemaia850@gmail.com>
2024-06-01 00:21:32 -0300

    ensure LoadUserByEmailRepository returns an user if user is found

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,9 @@ const config = {
   testEnvironment: 'node',
   collectCoverage: false,
   coverageDirectory: 'coverage',
-  collectCoverageFrom: ['**/src/**/*.js']
+  collectCoverageFrom: ['**/src/**/*.js'],
+  preset: '@shelf/jest-mongodb',
+  watchPathIgnorePatterns: ['globalConfig', 'node_modules']
 };
 
 module.exports = config;
